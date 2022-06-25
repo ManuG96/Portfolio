af3d3692e5df3f5fc05d170be0909c8b0a7ed52a
Sydri0n <manuelgarcia24696@gmail.com>
2022-06-25 15:19:46 -0300

    Update animation reset

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,38 +26,32 @@ function linkAction(){
 
 //Scroll reveal animation.
 const sr = ScrollReveal({
-    origin: "left",
-    distance: "50px",
-    duration: 800,
-    reset: true
+    origin: "top",
+    distance: "80px",
+    duration: 1000,
 })
 
 //Scroll reveal HOME.
 sr.reveal(".home__title",{});
-sr.reveal(".button",{delay: 100});
-sr.reveal(".home__img",{delay: 200});
-sr.reveal(".home__social-icon",{interval: 100});
-
+sr.reveal(".button",{delay: 200});
+sr.reveal(".home__img",{delay: 400});
+sr.reveal(".home__social-icon",{interval: 200});
 //Scroll reveal ABOUT.
 sr.reveal(".about__img",{});
-sr.reveal(".about__subtitle",{delay: 100});
-sr.reveal(".about__text",{delay: 200});
-
+sr.reveal(".about__subtitle",{delay: 200});
+sr.reveal(".about__text",{delay: 400});
 //Scroll reveal SKILLS.
 sr.reveal(".skills__subtitle",{});
-sr.reveal(".skills__text",{delay: 100});
-sr.reveal(".skills__data",{interval: 200});
-sr.reveal(".skills__img",{delay: 200});
-
+sr.reveal(".skills__text",{delay: 200});
+sr.reveal(".skills__data",{interval: 400});
+sr.reveal(".skills__img",{delay: 400});
 //Scroll reveal PROJECTS.
-sr.reveal(".projects__img",{interval: 100});
-
+sr.reveal(".projects__img",{interval: 200});
 //Scroll reveal CONTACT.
-sr.reveal(".contact__input",{interval: 100});
+sr.reveal(".contact__input",{interval: 200});
 
 
 //DARK LIGHT THEME
-
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-mode";
 const iconTheme = "bxs-sun";
